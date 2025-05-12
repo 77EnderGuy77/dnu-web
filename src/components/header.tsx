b1481c7158d0fba113eaca7a01b7aef8b213b695
77EnderGuy77 <134065941+77EnderGuy77@users.noreply.github.com>
2025-05-12 09:18:47 +0300

    Final version v2

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-
-import "../css/header.css";
-
 import { Link } from "react-router-dom";
+import "../css/header.css";
 
 import logo from "../assets/logo_top.svg";
 import pet_world from "../assets/pet_world_top.svg";
@@ -12,15 +10,22 @@ import login from "../assets/login.svg";
 export const Header: React.FC = () => {
   return (
     <div className="header">
-      <Link to="/" className="header-logo-text" style={{ cursor: "pointer" }}>
-        <div className="header-logo">
+      {/* Logo links to main page */}
+      <div className="header-logo">
+        <Link to="/" className="header-logo-link" style={{ cursor: "pointer" }}>
           <div className="header-logo-img">
             <img src={logo} alt="Pet World logo" />
           </div>
           <img src={pet_world} alt="Pet World" />
-        </div>
-        <p className="header-text">Оголошення</p>
-      </Link>
+        </Link>
+      </div>
+
+      {/* Text links to catalog */}
+      <div className="header-catalog">
+        <Link to="/catalog" className="header-text" style={{ cursor: "pointer" }}>
+          Оголошення
+        </Link>
+      </div>
 
       <div className="header-post-acc">
         <div className="header-ads-post">
